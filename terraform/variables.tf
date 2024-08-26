74a2f57ae0b149d7f607ab20ddbfb8ec38cf57b0
Diego Miranda <diego.miranda02@gmail.com>
2024-08-26 20:34:55 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,18 @@ variable "api_stage_name" {
   description = "The name of the API Gateway stage"
   default     = "dev"
 }
+
+variable "s3_bucket" {
+  type        = string
+  description = "The name of the S3 bucket to store the Terraform state"
+}
+
+variable "s3_key_path" {
+  type        = string
+  description = "The path to store the Terraform state in the S3 bucket"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "The AWS region for the resources"
+}
